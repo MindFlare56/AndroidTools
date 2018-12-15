283bbeee5d42e19e4f75630d3f3449465beb7f28
MindFlare56 <MindFlare56@hotmail.fr>
2018-12-14 19:50:47 -0500

    Creating more tools

--- app/src/main/java/com/mindf/utils/java/MainActivity.java
+++ app/src/main/java/com/mindf/utils/java/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 import com.mindf.utils.android.Dialog;
+import com.mindf.utils.android.ViewTools;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,11 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Dialog() {
-            @Override
-            protected void onPositiveButton() {
-                Toast.makeText(MainActivity.this, "Yes", Toast.LENGTH_SHORT).show();
-            }
-        }.createConfirmationDialog(this, "Are you sure?", "title").show();
+        ViewTools.logv(DateTime.getADateMilliSecondsValue("2018-12-14"));
     }
 }
